9ad7c53de8966eeb1411bbace28e36a939457116
Arturo Barrios <>
2022-04-20 20:12:40 -0400

    aws amplify authentication

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:amplify_flutter/amplify_flutter.dart';
+import 'package:amplify_authenticator/amplify_authenticator.dart';
 import 'package:amplify_auth_cognito/amplify_auth_cognito.dart';
 
 import 'amplifyconfiguration.dart';
@@ -55,7 +56,12 @@ class _MyAppState extends State<MyApp> {
       child: Builder(builder: (context) {
         // Save a context our Commands can use to access provided Models and Services
         Commands.init(context);
-        return MaterialApp(home: AppScaffold());
+        return 
+        Authenticator(
+          child:  MaterialApp(
+            builder: Authenticator.builder(),
+            home: AppScaffold()
+            ));
       }),
     );
   }
