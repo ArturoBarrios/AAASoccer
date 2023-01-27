66647cd2c80f7a87af8ec9cdebaea279dc49f88d
Arturo Barrios <>
2023-01-27 18:08:56 -0500

    adding card form stripe

--- lib/blocs/payment/payment_bloc.dart
+++ lib/blocs/payment/payment_bloc.dart
@@ -1,8 +1,9 @@
+import 'dart:convert';
 import 'package:bloc/bloc.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:equatable/equatable.dart';
 import 'package:flutter_stripe/flutter_stripe.dart';
-
+import 'package:http/http.dart' as http;
 part 'payment_event.dart';
 part 'payment_state.dart';
 
@@ -34,19 +35,71 @@ class PaymentBloc extends Bloc<PaymentEvent, PaymentState> {
      ),
     );
 
-    final paymentIntentResults = _callPayEndpointMethodId(
+    final paymentIntentResults = await _callPayEndpointMethodId(
       useStripeSdk: true,
       paymentMethodId: paymentMethod.id,
       currency: 'usd', 
       items: event.items,
     );
 
+    if(paymentIntentResults['error'] != null) {
+      emit(state.copyWith(status: PaymentStatus.failure));
+    }
+
+    if(paymentIntentResults['clientSecret'] != null &&
+      paymentIntentResults['requiresAction'] == null){
+      emit(state.copyWith(status: PaymentStatus.success));
+    }
+
+    if(paymentIntentResults['clientSecret'] != null &&
+      paymentIntentResults['requiresAction'] == true){
+        final String clientSecret = paymentIntentResults['clientSecret'];
+        add(PaymentConfirmIntent(clientSecret: clientSecret));
+      }
+
   }
   
   void _onPaymentConfirmIntent(
     PaymentConfirmIntent event,
     Emitter<PaymentState> emit,
-  ) {}
+  ) async {
+    try{
+      final paymentIntent = await Stripe.instance.handleNextAction(event.clientSecret);
+
+      if(paymentIntent.status == PaymentIntentsStatus.RequiresConfirmation){
+        Map<String, dynamic> results = await _callPayEndpointIntentId(
+          paymentIntentId: paymentIntent.id,
+        );
+        if(results['error'] != null){
+          emit(state.copyWith(status: PaymentStatus.failure));
+        }else{
+          emit(state.copyWith(status: PaymentStatus.success));
+        }
+
+      }
+      
+    } catch (e) {
+      print("e");
+      emit(state.copyWith(status: PaymentStatus.failure));
+    }
+  }
+
+
+  Future<Map<String, dynamic>> _callPayEndpointIntentId({
+    required String paymentIntentId
+  }) async {
+    final url = Uri.parse("https://us-central1-soccer-app-a9060.cloudfunctions.net/StripePayEndpointIntentId");
+    final response = await http.post(
+      url,
+      headers: {'Content-Type': 'application/json'},
+      body: json.encode(
+        {
+          'paymentIntentId': paymentIntentId,
+        }
+      )
+    );
+    return json.decode(response.body);
+  }
 
   Future<Map<String, dynamic>> _callPayEndpointMethodId({
     required bool useStripeSdk,
@@ -54,7 +107,7 @@ class PaymentBloc extends Bloc<PaymentEvent, PaymentState> {
     required String currency,
     required List<Map<String, dynamic>> items,
   }) async {
-    final url = Uri.parse("https://us-central1-soccer-app-a9060.cloudfunctions.net/StripePayEndpointIntentId");
+    final url = Uri.parse("https://us-central1-soccer-app-a9060.cloudfunctions.net/StripePayEndpointMethodId");
     final response = await http.post(
       url,
       headers: {'Content-Type': 'application/json'},
