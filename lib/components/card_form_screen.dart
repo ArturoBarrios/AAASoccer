22343874ef1adb56f2b13e3f80d2d794c37999e2
Arturo Barrios <>
2023-01-28 12:09:06 -0500

    card form screen, created from tutorial(https://www.youtube.com/watch?v=tpILK64NM6M&t=795s)

--- lib/components/card_form_screen.dart
+++ lib/components/card_form_screen.dart
@@ -1,6 +1,7 @@
 import 'dart:ui';
 import 'package:flutter_stripe/flutter_stripe.dart';
 import 'package:flutter/material.dart';
+import 'package:soccermadeeasy/blocs/payment/payment_bloc.dart';
 import '../services/stripe_service.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
@@ -22,6 +23,9 @@ class _CardFormScreen
       appBar: AppBar(),
       body: BlocBuilder<PaymentBloc, PaymentState>(
         builder: (context, state) {
+          CardFormEditController controller = CardFormEditController(
+            initialDetails: state.cardFieldInputDetails,
+          );
           if( state.status== PaymentStatus.initial){
             return Padding(
               padding: const EdgeInsets.all(20),
@@ -35,19 +39,56 @@ class _CardFormScreen
                   ),
                   const SizedBox(height: 20),
                   CardFormField(
-                    controller: CardFormEditController(),
+                    controller: controller,
                   ),
                   const SizedBox(height: 10),
                   ElevatedButton(
-                    onPressed: () {},
+                    onPressed: () {
+                      (controller.details.complete)
+                        ? context.read<PaymentBloc>().add(
+                            const PaymentCreateIntent(
+                              billingDetails: BillingDetails(
+                                email: 'testingflutter@dev.com'
+                              ),
+                              items: [
+                                {'id': 0},
+                                {'id': 1}
+                              ],
+                            ),
+                          )
+                            : ScaffoldMessenger.of(context).showSnackBar(
+                              const SnackBar(
+                                content: Text('The form is not complete.'),
+                              ),
+                            );                        
+                    },
                     child: const Text('Pay')
                   )
                 ]
               )
             );
           }
-          if(state.status)
+          if(state.status == PaymentStatus.success){
+            return Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                const Text('The payment is successful.'),
+                const SizedBox(
+                  height: 10,
+                  width: double.infinity,
+                ),
+                ElevatedButton(
+                  onPressed: () {
+                    context.read<PaymentBloc>().add(PaymentStart());
+                  },
+                  child: const Text('Back to Home')
+                ),
+              ],
+            );
+            
+          }
           else{
+            return const Center(child: CircularProgressIndicator());
 
           }
 
