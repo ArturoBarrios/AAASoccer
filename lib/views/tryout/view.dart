3d3a187fca9058a7dfec32f7a66328038546e53d
Arturo Barrios <arturo.barrios@theplanworks.com>
2023-09-16 21:55:30 -0400

    tryout refactoring

--- lib/views/tryout/view.dart
+++ lib/views/tryout/view.dart
@@ -1,16 +1,31 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_animated_dialog/flutter_animated_dialog.dart';
+import 'package:provider/provider.dart';
 import 'package:soccermadeeasy/components/price_widget.dart';
 
 import '../../commands/event_command.dart';
 import '../../components/Loading/loading_screen.dart';
 import '../../components/Mixins/event_mixin.dart';
 import '../../components/Mixins/images_mixin.dart';
+import '../../components/chats_list_widget.dart';
+import '../../components/event_date_widget.dart';
+import '../../components/get_join_event_widget.dart';
 import '../../components/headers.dart';
+import '../../components/images_list_widget.dart';
+import '../../components/location_search_bar.dart';
 import '../../components/my_map_page.dart';
 import '../../components/object_profile_main_image.dart';
+import '../../components/payment_list_widget.dart';
+import '../../components/players_list_widget.dart';
+import '../../components/requests_list.dart';
+import '../../components/rsvp_widget.dart';
+import '../../components/send_players_request_widget.dart';
+import '../../components/send_teams_request_widget.dart';
+import '../../components/teams_list_widget.dart';
 import '../../components/update_view_form.dart';
 import '../../constants.dart';
+import '../../models/enums/payment_type.dart';
+import '../../models/pageModels/event_page_model.dart';
 
 class TryoutView extends StatefulWidget with EventMixin, ImagesMixin {
   TryoutView({Key? key, required this.tryout}) : super(key: key);
@@ -29,23 +44,11 @@ class _TryoutViewState extends State<TryoutView> {
   final surfaceController = TextEditingController();
   final fieldSizeController = TextEditingController();
   final privateController = TextEditingController();
-
-  dynamic userEventDetails;
-
-  dynamic objectImageInput = {
-    "imageUrl": "",
-    "containerType": Constants.IMAGEBANNER,
-    "mainEvent": null,
-    "isMyEvent": false
-  };
+  
+LocationSearchBar locationSearchBar = new LocationSearchBar();
+  
 
   bool _isLoading = true;
-
-  void updateChatsList(dynamic createdChat) {
-    setState(() {
-      userEventDetails['mainEvent']['chats']['data'].add(createdChat);
-    });
-  }
 
   void goBack() {
     Navigator.pop(context);
@@ -58,32 +61,21 @@ class _TryoutViewState extends State<TryoutView> {
   }
 
   List<int>? selectedRequestTypeIndexes;
-  List requestUserTypes = [
-    Constants.PLAYER.toString(),
-    Constants.ORGANIZER.toString(),
-    Constants.MANAGER.toString(),
-    Constants.MAINCOACH.toString(),
-    Constants.ASSISTANTCOACH.toString(),
-    Constants.REF.toString(),
-  ];
+  
   List<String> selectedRequestTypeObjects = [];
 
   Future<void> loadInitialData() async {
     print("loadInitialData");
     dynamic getEventDetailsResp =
-        await EventCommand().getUserEventDetails([widget.tryout]);
-    userEventDetails = getEventDetailsResp;
-    widget.setupPlayerList();
-    print("getEventDetailsResp: " + getEventDetailsResp.toString());
-    //setup image
-    objectImageInput = await widget.loadEventMainImage(userEventDetails);
+        await EventCommand().getUserEventDetails([widget.tryout]);    
+    widget.setupPlayerList();        
+    
     //wait for 3 seconds
     await Future.delayed(const Duration(seconds: 2));
     setState(() {
       _isLoading = false;
       print("setState finished");
-    });
-    loadEventPayment();
+    });    
     print("loadInitialData finished");
   }
 
@@ -97,6 +89,31 @@ class _TryoutViewState extends State<TryoutView> {
 
   @override
   Widget build(BuildContext context) {
+    dynamic mainEvent =
+        context.select<EventPageModel, dynamic>((value) => value.mainEvent);
+    dynamic objectImageInput = context.watch<EventPageModel>().objectImageInput;
+    List<dynamic> roles =
+        context.select<EventPageModel, List<dynamic>>((value) => value.roles);
+    bool isMine = context.select<EventPageModel, bool>((value) => value.isMine);
+    bool isMember =
+        context.select<EventPageModel, bool>((value) => value.isMember);
+    dynamic price =
+        context.select<EventPageModel, dynamic>((value) => value.price);
+    String amountRemaining = context
+        .select<EventPageModel, String>((value) => value.amountRemaining);
+    String amountPaid =
+        context.select<EventPageModel, String>((value) => value.amountPaid);
+    String teamAmountRemaining = context
+        .select<EventPageModel, String>((value) => value.amountRemaining);
+    String teamAmountPaid =
+        context.select<EventPageModel, String>((value) => value.teamAmountPaid);
+    List userParticipants =
+        context.select<EventPageModel, List>((value) => value.userParticipants);
+    List teams = context.select<EventPageModel, List>((value) => value.teams);
+    List players =
+        context.select<EventPageModel, List>((value) => value.players);
+    List chats = context.watch<EventPageModel>().chats;
+    List payments = context.watch<EventPageModel>().payments;
     return Scaffold(
         appBar: PreferredSize(
           preferredSize: Size.fromHeight(
@@ -118,69 +135,132 @@ class _TryoutViewState extends State<TryoutView> {
                 ),
               )
             : ListView(padding: EdgeInsets.all(16), children: [
-                !userEventDetails['isMine']
-                    ? Container(
-                        height: 20,
-                        child: ClipRRect(
-                          borderRadius: BorderRadius.circular(20.0),
-                          child: GestureDetector(
-                              onTap: () async {
-                                print("onTap: ");
-                                List<int>? requestIndexes =
-                                    await showAnimatedDialog<dynamic>(
-                                  context: context,
-                                  barrierDismissible: true,
-                                  builder: (BuildContext context) {
-                                    return ClassicListDialogWidget<dynamic>(
-                                        selectedIndexes:
-                                            widget.selectedRequestTypeIndexes,
-                                        titleText: 'Choose User Type',
-                                        positiveText: "Send Request",
-                                        listType: ListType.multiSelect,
-                                        activeColor: Colors.green,
-                                        dataList: widget.requestUserTypes);
-                                  },
-                                  animationType: DialogTransitionType.size,
-                                  curve: Curves.linear,
-                                );
-
-                                widget.selectedRequestTypeIndexes =
-                                    requestIndexes ??
-                                        widget.selectedRequestTypeIndexes;
-                                print(
-                                    'selectedIndex:${widget.selectedRequestTypeIndexes?.toString()}');
-                                await widget.requestTypeSelected(
-                                    widget.selectedRequestTypeIndexes);
-                                await widget.sendEventRequest(widget.tryout,
-                                    {0: {}}, widget.requestUserTypes, []);
-                              },
-                              child: Text("Send Request")),
-                        ),
-                      )
-                    : Container(),
-                UpdateViewForm(userObjectDetails: userEventDetails)
-                //    Row(
-                //       mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                //       children: [
-                //         widget.getPriceWidget(widget.userEventDetails),
-                //         // Text(
-                //         //     "Price: \$${(double.parse(priceObject['amount']) / 100).toStringAsFixed(2)}"),
-                //         widget.userEventDetails['isMine']
-                //             ? ElevatedButton(
-                //                 onPressed: () {
-                //                   // Add button onPressed logic here
-                //                 },
-                //                 child: Text('Update Payment'),
-                //               )
-                //             : Container(),
-                //       ],
-                //     ),
-                //     widget.getJoinGameWidget(context, widget.userEventDetails, widget.tryout['event'], widget.userObject),
-                //     widget.getChatWidget(context, true, false, widget.userEventDetails, updateChatsList),
-
-                //     widget.userEventDetails['isMine']
-                // ? widget.sendPlayersRequestWidget(context, widget.userEventDetails)
-                // : widget.sendOrganizerPlayerEventRequest(context, widget.userEventDetails),
+              IconButton(
+                  icon: const Icon(
+                    Icons.share,
+                    color: Colors.blue,
+                  ),
+                  onPressed: () async {
+                    await EventCommand().onTapShare(mainEvent['mainImageKey']);
+                  }
+                ),
+                IconButton(
+                  icon: const Icon(
+                    Icons.social_distance,
+                    color: Colors.red,
+                  ),
+                  onPressed: () async {
+                    await EventCommand().onTapSocialMediaApp(context); 
+                  } 
+                ),
+                //date
+                EventDateWidget(
+                    canEdit: isMine,
+                    startTime: mainEvent['startTime'],
+                    endTime: mainEvent['endTime']),
+                //map
+                Container(
+                  margin: const EdgeInsets.all(10.0),
+                  color: Colors.amber[600],
+                  width: MediaQuery.of(context).size.width -
+                      (MediaQuery.of(context).size.width * .1), //10% padding
+                  height: 200.0,
+                  child: MyMapPage(
+                      latitude: mainEvent['location']['data'][0]['latitude'],
+                      longitude: mainEvent['location']['data'][0]['longitude']),
+                ),
+                //join widget
+                GetJoinEventWidget(
+                    mainEvent: mainEvent,
+                    roles: roles,
+                    isMine: isMine,
+                    price: price,
+                    amountRemaining: amountRemaining),
+                //Requests widget
+                RequestsList(
+                    objectDetails: {"requests": mainEvent['requests']['data']}),
+                //RSP
+                RSVPWidget(
+                  event: mainEvent,
+                  userParticipants: userParticipants,
+                ),
+                //location search bar
+                locationSearchBar = LocationSearchBar(
+                    initialValue: mainEvent['location']['data'][0]['name']),
+                //player list
+                PlayerList(
+                  event: mainEvent,
+                  team: null,
+                  userParticipants: widget.modifiedParticipantList(
+                      userParticipants, payments, price['amount']),
+                  inviteUserToChat: (final userId) async =>
+                      widget.onTapShowChatBottomSheet(
+                          context: context, chatList: chats, userId: userId),
+                ),
+                //team list
+                TeamsListWidget(user: null, mainEvent: mainEvent, teams: teams),
+                //player request widget
+                SendPlayersRequestWidget(
+                    mainEvent: mainEvent,
+                    team: null,
+                    players: players,
+                    isMine: isMine),
+                //team request widget
+                SendTeamsRequestWidget(
+                  mainEvent: mainEvent,
+                  isMine: isMine,
+                  teams: teams,
+                  addTeamCallback: EventCommand().addTeamCallback,
+                ),
+                //chat widget
+                // GetChatWidget(
+                //     mainEvent: mainEvent,
+                //     team: null,
+                //     players: players,
+                //     isMine: isMine,
+                //     updatechatsList: updateChatsList),
+                ChatsListWidget(
+                  chats: chats,
+                ),
+                // Player price widget
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    PriceWidget(
+                        price: price,
+                        teamPrice: false,
+                        eventPrice: true,
+                        amountPaid: amountPaid,
+                        amountRemaining: amountRemaining,
+                        isMine: isMine,
+                        isMember: isMember)
+                  ],
+                ),
+                //Team price widget
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    PriceWidget(
+                        price: price,
+                        teamPrice: true,
+                        eventPrice: false,
+                        amountPaid: teamAmountPaid,
+                        amountRemaining: teamAmountRemaining,
+                        isMine: isMine,
+                        isMember: isMember)
+                  ],
+                ),
+                const SizedBox(height: 20),
+                PaymentListWidget(
+                  paidUsers: widget.getPaidUsers(userParticipants, payments),
+                  paymentType: PaymentType.user,
+                ),
+                const SizedBox(height: 60),
+                ImagesListWidget(
+                    mainEvent: mainEvent,
+                    team: null,
+                    imageFor: Constants.EVENT),
+                
               ]));
   }
 }
